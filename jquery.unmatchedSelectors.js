2ef9542baefa416d8bb1cb55e8f8e4f8cc8c8681
Kai Dorschner <the-kernel32@web.de>
2012-10-05 16:28:45 +0200

    Made it jquery.plugin compliant.

--- jquery.unmatchedSelectors.js
+++ jquery.unmatchedSelectors.js
@@ -5,23 +5,23 @@
  * @author Kai Dorschner <the-kernel32@web.de>
  * @todo Use an option to enable different logger callbacks like jquery ui badges or something.
  */
-(function () {
+(function ($) {
 	"use strict";
+	var UnmatchedSelector;
+	if ($) {
+		UnmatchedSelector = function (selector, context) {
+			this.stack = (new Error()).stack.split('\n');
+			this.message = '$("' + selector + '"); didn\'t match any content' + (context ? ' in context ' + context : '') + ' - ' + $.trim(this.stack[this.stack.length - 1]);
+			this.selector = selector;
+			this.context = context;
+		};
+		UnmatchedSelector.prototype = new Error();
+		UnmatchedSelector.prototype.constructor = UnmatchedSelector;
+		UnmatchedSelector.prototype.name = 'UnmatchedSelector';
 
-	var UnmatchedSelector = function (selector, context) {
-		this.stack = (new Error()).stack.split('\n');
-		this.message = '$("' + selector + '"); didn\'t match any content' + (context ? ' in context ' + context : '') + ' - ' + $.trim(this.stack[this.stack.length - 1]);
-		this.selector = selector;
-		this.context = context;
-	};
-	UnmatchedSelector.prototype = new Error();
-	UnmatchedSelector.prototype.constructor = UnmatchedSelector;
-	UnmatchedSelector.prototype.name = 'UnmatchedSelector';
-
-	if (window.jQuery) {
-		jQuery.fn._init = jQuery.fn.init;
-		jQuery.fn.init = function (selector, context) {
-			var result = new jQuery.fn._init(selector, context),
+		$.fn._init = $.fn.init;
+		$.fn.init = function (selector, context) {
+			var result = new $.fn._init(selector, context),
 				error = null;
 			if (selector !== undefined && !result.length) {
 				error = new UnmatchedSelector(selector, context);
@@ -34,4 +34,4 @@
 			return result;
 		};
 	}
-}());
+}(jQuery));
