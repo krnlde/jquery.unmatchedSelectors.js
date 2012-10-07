23970960bd15cad62da58195b95f40655624426d
Kai Dorschner <the-kernel32@web.de>
2012-10-07 14:59:02 +0200

    Fixed the issue when not providing a rootQuery while calling $.fn.init. Added custom logger Callback

--- jquery.unmatchedSelectors.js
+++ jquery.unmatchedSelectors.js
@@ -1,35 +1,41 @@
 /**
  * jQuery Unmatched Selectors Plugin
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @author Kai Dorschner <the-kernel32@web.de>
  * @todo Use an option to enable different logger callbacks like jquery ui badges or something.
  */
 (function ($) {
 	"use strict";
 	var UnmatchedSelector;
+	$.extend($, {
+		unmatchedSelectorLogger: function (e) {
+			if (!console) return;
+			if (console.warn) {
+				console.warn(e);
+			} else if (console.log) {
+				console.log(e);
+			}
+		}
+	});
+
 	if ($) {
 		UnmatchedSelector = function (selector, context) {
 			this.stack = (new Error()).stack.split('\n');
-			this.message = '$("' + selector + '"); didn\'t match any content' + (context ? ' in context ' + context : '') + ' - ' + $.trim(this.stack[this.stack.length - 1]);
+			this.file = $.trim(this.stack[this.stack.length - 1]);
 			this.selector = selector;
 			this.context = context;
+			this.message = '$("' + this.selector + '"); didn\'t match any content' + (this.context ? ' in context ' + this.context : '') + ' - ' + this.file;
 		};
 		UnmatchedSelector.prototype = new Error();
 		UnmatchedSelector.prototype.constructor = UnmatchedSelector;
 		UnmatchedSelector.prototype.name = 'UnmatchedSelector';
 
-		$.fn._init = $.fn.init;
-		$.fn.init = function (selector, context) {
-			var result = new $.fn._init(selector, context),
-				error = null;
+		$.fn._init = $.fn.init; // Preserve the original init method
+		$.fn.init = function (selector, context, rootQuery) { // overwrite / extend the original init method
+			var result = new $.fn._init(selector, context, rootQuery || window.document);
 			if (selector !== undefined && !result.length) {
-				error = new UnmatchedSelector(selector, context);
-				if (console.warn) {
-					console.warn(error);
-				} else if (console.log) {
-					console.log(error);
-				}
+				$.unmatchedSelectorLogger.call(null, new UnmatchedSelector(selector, context));
 			}
 			return result;
 		};
